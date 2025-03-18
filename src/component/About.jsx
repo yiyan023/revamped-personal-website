ea3909c3f64147cc0af343ca0c6663d772c76144
Yiyan Huang <y84huang@uwaterloo.ca>
2025-03-17 21:11:34 -0400

    finished most of about page

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,13 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
+import { useAppContext } from '../AppContext'
+import './styling/About.css'
+import Yiyan from '../assets/imgs/yiyan.png'
 
 const About = () => {
-  return (
-	<div>
-		<Navbar />
-	  About
-	</div>
-  )
+	const { session } = useAppContext();
+	const [ idx, setIdx ] = useState(0);
+	const [ cIdx, setCIdx ] = useState(0);
+	const [ displayedText, setDisplayedText ] = useState("")
+
+	const rotatedText = ["software developer", "dreamer", "innovator"];
+
+
+	useEffect(() => {
+		let charTimer;
+	
+		if (cIdx < rotatedText[idx]?.length) {
+			charTimer = setTimeout(() => {
+				setDisplayedText((prev) => prev + rotatedText[idx][cIdx]);
+				setCIdx(cIdx + 1);
+			}, 100); 
+		} else {
+			const wordTimer = setTimeout(() => {
+				setIdx((prevIdx) => (prevIdx + 1) % rotatedText.length);
+				setDisplayedText("");
+				setCIdx(0);
+			}, 1500);
+	
+			return () => clearTimeout(wordTimer);
+		}
+	
+		return () => clearTimeout(charTimer);
+	}, [idx, cIdx]);
+	
+	
+
+	return (
+		<div className='about'>
+			<div className='info'>
+				<Navbar />
+				<h1>hi <span className='highlight'>{session ? session : "there"}</span> 👋, i'm <span className='highlight'>yiyan</span></h1>
+				<h3 className='roles'>{displayedText}</h3>
+				<p className='links'>
+					<a target="_blank" href='https://drive.google.com/file/d/13hMotHEbqZqr-Rs-L3cPDaQ0vRvZuK5n/view?usp=sharing'>cv</a> · <a target="_blank" href='https://www.linkedin.com/in/yiyanhh/'>linkedin</a> · <a target="_blank" href='mailto:y84huang@uwaterloo.ca'>email</a> · <a target="_blank" href='https://github.com/yiyan023'>github</a>
+				</p>
+				<p className='interests'>k-drama, pokemon & badminton enthusiast</p>
+			</div>
+			<div className='img'>
+				<img src={Yiyan}/>
+			</div>
+		</div>
+	)
 }
 
 export default About
--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Navbar from './Navbar'
+import { useAppContext } from '../AppContext'
+import './styling/Login.css'
+import { FaArrowRight } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
-  return (
-	<div>
-		<Navbar />
-	  Login
-	</div>
-  )
+	const { setSession } = useAppContext();
+	const navigate = useNavigate();
+	const buttonRef = useRef(null);
+	const inputRef = useRef(null);
+	const [ input, setInput ] = useState("")
+
+	const login = () => {
+		setSession(input);
+		navigate('/home');
+	}
+
+	useEffect(() => {
+		inputRef.current?.focus();
+	}, [])
+
+	useEffect(() => {
+		const handleKeyDown = (event) => {
+            if (event.key === 'Enter') {
+                buttonRef.current.click();
+            }
+        };
+
+		window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+	}, [])
+
+	return (
+		<div>
+			<Navbar />
+			<h1>hi there</h1>
+			<div className='login-div'>
+				<input type="text" placeholder="what is your name?" value={input} onChange={(e) => {setInput(e.target.value)}} ref={inputRef}/>
+				<button className="login-btn" onClick={login} ref={buttonRef}><FaArrowRight /></button>
+			</div>
+		</div>
+	)
 }
 
 export default Login
